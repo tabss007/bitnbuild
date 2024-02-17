b3458690d1b8c64328f237e59820ee168b669663
ISHA3004 <117744943+ISHA3004@users.noreply.github.com>
2024-02-17 17:44:37 +0530

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-const connection = require("./connection");
-const express = require("express");
-const app = express();
-const cors = require('cors');
-const port = 5000;
-
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-  })
